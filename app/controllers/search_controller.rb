041a2d22b8562d30e9c78be67eb1d63b4bca428a
BFunk <bret.e.funk@gmail.com>
2017-09-13 11:11:42 -0600

    it works, now to build service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def index
-    zipcode = params[:search]
-    zipcode = "80202"
-    @conn = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=2&apiKey=#{ENV['API_KEY']}")
+    @zipcode = params[:search]
+    @zipcode = "80202"
+    #^^ for testing
+    @conn = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{@zipcode},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=2&apiKey=#{ENV['API_KEY']}")
+    @quantity = JSON.parse(@conn.body, symbolize_names: true)[:total]
     @response = JSON.parse(@conn.body, symbolize_names: true)[:stores]
     @locations = @response.map do |raw_location|
       Location.new(raw_location)
