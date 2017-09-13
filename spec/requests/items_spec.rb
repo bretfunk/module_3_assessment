c034e62931c35d6061b4b169e871d43252c5081c
BFunk <bret.e.funk@gmail.com>
2017-09-13 09:09:11 -0600

    item api get test

---
+++ spec/requests/items_spec.rb
@@ -0,0 +1,26 @@
+#When I send a GET request to `/api/v1/items`
+#I receive a 200 JSON response containing all items
+#And each item has an id, name, description, and image_url but not the created_at or updated_at
+
+#When I send a GET request to `/api/v1/items/1`
+#I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+
+#When I send a DELETE request to `/api/v1/items/1`
+#I receive a 204 JSON response if the record is successfully deleted
+
+#When I send a POST request to `/api/v1/items` with a name, description, and image_url
+#I receive a 201 JSON  response if the record is successfully created
+#And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+
+#* Verify that your non-GET requests work using Postman or curl. (Hint: `ActionController::API`). Why doesn't the default `ApplicationController` support POST and PUT requests?
+require 'rails_helper'
+
+describe "Items API" do
+  it "returns all items" do
+    create_list(:item, 3)
+    get "/api/v1/items"
+    expect(response).to be_success
+    items = JSON.parse(response.body)
+    expect(items.count).to eq(3)
+  end
+end
