de83efe31991b0db09b84d74a3424db8174d3b8c
BFunk <bret.e.funk@gmail.com>
2017-09-13 10:59:10 -0600

    setting up locations model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,9 @@ class SearchController < ApplicationController
     zipcode = params[:search]
     zipcode = "80202"
     @conn = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=2&apiKey=#{ENV['API_KEY']}")
+    @response = JSON.parse(@conn.body, symbolize_names: true)[:stores]
+    @locations = @response.map do |raw_location|
+      Location.new(raw_location)
+    end
   end
 end
---
+++ app/models/location.rb
@@ -0,0 +1,6 @@
+class Location
+  attr_reader :long_name, :city, :distance, :phone, :type
+  def initialize(attrs)
+    byebug
+  end
+end
